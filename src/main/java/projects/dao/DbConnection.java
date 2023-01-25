80f201499a8c874a72233d724851649445efc1cb
Marisa Davidson <marisalynnae97@gmail.com>
2023-01-24 21:06:36 -0600

    week 8 commit

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -14,15 +14,15 @@ public class DbConnection {
 	private static String USER = "projects";
 	
 	public static Connection getConnection() {
-		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", 
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", 
 				HOST, PORT, SCHEMA, USER, PASSWORD);
 		try {
-			Connection conn = DriverManager.getConnection(uri);
+			Connection conn = DriverManager.getConnection(url);
 			System.out.println("Connection to schema '" + SCHEMA + "' is successful.");
 			return conn;
 		} catch (SQLException e) {
-			System.out.println("Unable to get connection at " + uri);
-			throw new DbException("Unable to get connection at \" + uri");
+			System.out.println("Unable to get connection at " + url);
+			throw new DbException("Unable to get connection at \" + url");
 		}
 		
 	}
